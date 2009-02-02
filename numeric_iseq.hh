69167af642e834c5577208450ea463fed8996d07
Matan Nassau <matan.nassau@gmail.com>
2009-02-02 02:18:17 -0500

    Added iseq overloads for all numeric algs
    adjacent_difference, inner_product and partial_sum.

--- numeric_iseq.hh
+++ numeric_iseq.hh
@@ -48,6 +48,52 @@ V accumulate(input_sequence_range<In> range, V init, BinOp op)
     return std::accumulate(range.first, range.second, init, op);
 }
 
+template<typename In, typename Out>
+Out adjacent_difference(input_sequence_range<In> range, Out res)
+{
+    return std::adjacent_difference(range.first, range.second, res);
 }
+
+template<typename In, typename Out, typename BinOp>
+Out adjacent_difference(input_sequence_range<In> range, Out res, BinOp op)
+{
+    return std::adjacent_difference(range.first, range.second, res, op);
+}
+
+template<typename In, typename In2, typename V>
+V inner_product(input_sequence_range<In> range, In2 first2, V init)
+{
+    return std::inner_product(range.first, range.second, first2, init);
+}
+
+template<typename In,
+         typename In2,
+         typename V,
+         typename BinOp1,
+         typename BinOp2>
+V inner_product(input_sequence_range<In> range,
+                In2 first2,
+                V init,
+                BinOp1 op1, BinOp2 op2)
+{
+    return std::inner_product(range.first, range.second,
+                              first2,
+                              init,
+                              op1, op2);
+}
+
+template<typename In, typename Out>
+Out partial_sum(input_sequence_range<In> range, Out res)
+{
+    return std::partial_sum(range.first, range.second, res);
+}
+
+template<typename In, typename Out, typename BinOp>
+Out partial_sum(input_sequence_range<In> range, Out res, BinOp op)
+{
+    return std::partial_sum(range.first, range.second, res, op);
+}
+
+} // namespace wt
 
 #endif // NUMERIC_ISEQ_HH_
