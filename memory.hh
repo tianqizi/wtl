c585954e3edb607e1fa00b097fab0b06e171bfc9
Matan Nassau <matan.nassau@gmail.com>
2010-12-27 01:02:41 -0500

    Added delete_ptr, delete_ptr_arr functors

--- memory.hh
+++ memory.hh
@@ -4,6 +4,15 @@
 #include <wtl/memory_iseq.hh>
 
 namespace wt {
+struct delete_ptr {
+    template<class P>
+    void operator()(P ptr) const { delete ptr; }
+};
+
+struct delete_ptr_arr {
+    template<class P>
+    void operator()(P ptr) const { delete[] ptr; }
+};
 } // namespace wt
 
 #endif // MEMORY_HH_
