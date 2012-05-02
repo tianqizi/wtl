77adbd8ef41a19e11fbea391f0f2ebf895d8b4c2
Matan Nassau <matan.nassau@gmail.com>
2012-05-01 23:14:19 -0400

    make_unique()

--- memory.hh
+++ memory.hh
@@ -2,6 +2,8 @@
 #define MEMORY_HH_
 
 #include <wtl/memory_iseq.hh>
+#include <memory>
+#include <utility>
 
 namespace wt {
 struct delete_ptr {
@@ -13,6 +15,12 @@ struct delete_ptr_arr {
     template<class P>
     void operator()(P ptr) const { delete[] ptr; }
 };
+
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
 } // namespace wt
 
 #endif // MEMORY_HH_
